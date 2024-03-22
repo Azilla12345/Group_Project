5984a42a6816a9efa785a2d6a971f2a3c0080d34
BT_1N3_13 <why@gmail.com>
2024-03-22 11:01:37 -0400

    Changes by BT_1N3_14

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,20 +1,26 @@
+import java.util.Scanner;
+
 public class Puzzle {
     int puzzleType;
     String answer;
     Prisoner prisoner;
-
-    int attempts;
-
+    Scanner myScanner = new Scanner(System.in);
 
     public Puzzle(int type) {
         puzzleType = type;
         if (type == 1) {
             puzzle1("");
         }
-        attempts = 0;
+
     }
 
+    public String move (String answer) {
+        return answer;
+    }
     public void puzzle1 (String answer) {
+        System.out.println("What is your next move?");
+        boolean chosen = false;
+        int attempts = 0;
         if (answer.equals(this.answer)) {
             System.out.println("Correct!");
         } else {
