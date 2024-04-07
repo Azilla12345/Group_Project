a8eab2b8bacc1c88cd5bb6ef01d1689eb3dbc6d8
hayde <why@gmail.com>
2024-04-07 10:36:55 -0400

    Code

--- src/Prison.java
+++ src/Prison.java
@@ -1,10 +1,20 @@
 public class Prison {
-    int guards;
-    int floors;
 
-    public Prison(int floors) {
-        this.floors = floors;
-        guards = (int)(Math.random() * 5) + 1;
+    String[] schedule = new String[5];
+    int guards;
+    int day;
+
+    public Prison(int day) {
+        this.day = day;
+        schedule[0] = "Breakfast";
+        schedule[1] = "work";
+        schedule[2] = "Lunch";
+        schedule[3] = "Free Time";
+        schedule[4] = "Dinner";
+    }
+
+    public String getSchedule(int moment) {
+        return schedule[moment];
     }
 
 
--- src/PrisonRunner.java
+++ src/PrisonRunner.java
@@ -6,7 +6,7 @@ public class PrisonRunner {
         System.out.println("You wake up in a ventilated prison cell, guarded by five guards.");
         System.out.println("As you grip your screwdriver and look around in confusion, you notice a mark on a calender.");
         System.out.println("The date February 11th is marked as your \"execution\"");
-        System.out.println("Its now or never. Its time for you to escape or die trying.");
+        System.out.println("7 days.  That's all the time you have left");
 
     }
 }
